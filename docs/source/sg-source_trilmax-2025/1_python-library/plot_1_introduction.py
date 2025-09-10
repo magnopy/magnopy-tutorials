a2e2bace0b8cd6ea61168c0599bf7f7c6ab9afa5
Andrey Rybakov <rybakov.ad@icloud.com>
2025-09-10 10:09:34 +0200

    REfactor

--- docs/source/sg-source_trilmax-2025/1_python-library/plot_1_introduction.py
+++ docs/source/sg-source_trilmax-2025/1_python-library/plot_1_introduction.py
@@ -8,29 +8,30 @@ Introducing magnopy
     *   Check that you have version 0.2.0 available
 
 First of all, |magnopy|_ shall be installed. For the full guide on installation you can
-read |magnopy-installation|_. For using magnopy in the school you need to install magnopy right within the
-jupyter notebook. Type the following command in the free notebook cell, run it and restart
-the kernel if necessary.
+read |magnopy-installation|_. For using magnopy in this part of the tutorial you need to
+install it right within the jupyter notebook. Type the following command in the free
+notebook cell, run it and restart the kernel if necessary.
 
 .. code-block:: bash
 
     %pip install "magnopy[visual]" --upgrade
 
 Note that you are installing the extended version of magnopy that supports production of
-``.png`` and ``.html`` files, that will be mentioned in this tutorial.
+``.png`` and ``.html`` files. Those file will be mentioned in this tutorial.
 
 .. note::
 
     We recommend to install magnopy with its visual capabilities whenever possible.
-    Magnopy will output graphics by default if |plotly|_ and |magnopy|_ are installed.
-    Nevertheless, we did not want to include graphical libraries as default dependencies
-    to the package.
+    Magnopy will output graphics by default if |plotly|_ and |matplotlib|_ are installed.
+    Nevertheless, graphical libraries are not included as default dependencies
+    to the package in order to offer computational capabilities of magnopy even if those
+    libraries are unaccessible.
 
 
 Import
 ======
 
-Magnopy have a well defined set of public functions, that shall be available in code
+Magnopy has a well defined set of public functions, that shall be available with code
 completion. Full list of available methods and objects one can always find on
 |magnopy-API|_.
 
@@ -46,8 +47,7 @@ import magnopy
 #
 #     It is enough to import magnopy once at the top of the notebook.
 #
-# To check that installation worked properly try to execute in the notebook cell the following
-# code
+# To check that installation worked properly try to execute the following
 
 print(magnopy.logo())
 
