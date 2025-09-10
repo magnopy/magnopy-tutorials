4ea1413cd3c190514c794d69df4638fc56dee3cd
Andrey Rybakov <rybakov.ad@icloud.com>
2025-09-10 10:26:58 +0200

    Refactor

--- docs/source/sg-source_trilmax-2025/1_python-library/plot_2_crystal.py
+++ docs/source/sg-source_trilmax-2025/1_python-library/plot_2_crystal.py
@@ -7,15 +7,19 @@ Crystal structure
     *   Create a crystal structure for the material of your choosing.
         Create a cell and a set of atoms. Specify all mentioned properties for each atom.
     *   (extra) Visualize your structure using :external:py:class:`magnopy.PlotlyBackend`
+    *   (extra) Get conventional and primitive cell of your structure (see
+        :external:py:func:`wulfric.crystal.get_primitive` and
+        :external:py:func:`wulfric.crystal.get_conventional`). Visualize them. Are they
+        they the same for your crystal? Shall they be the same always or not?
 
 Cell and atoms
 ==============
 
-At the heart of every spin Hamiltonian lies some crystal structure that defined the unit
-cell of the lattice and set of magnetic sites.
+There is some crystal structure at the heart of every spin Hamiltonian. It defined the
+unit cell of the lattice and set of magnetic sites.
 
-Unit cell is simply a set of three vectors, that define the basis of the crystal. Magnopy
-stores it in the same way as |wulfric|_ and as many other Python codes
+Unit cell is simply a set of three vectors. Magnopy stores it in the same way as
+|wulfric|_ and as many other Python codes do
 """
 
 import magnopy
@@ -29,8 +33,8 @@ cell = [
 # %%
 # To read more about the cell see |magnopy-cell|_.
 #
-# Magnopy calls magnetic centers ``atoms`` due to historical reasons, but they are not
-# necessary atoms. We will use the term atoms in this tutorial. In all cases the atoms will
+# Magnopy calls magnetic centers "atoms" due to historical reasons, but they are not
+# necessary atoms. We will use the term "atoms" in this tutorial. In all cases "atoms"
 # simply mean an object that has a set of properties defined
 #
 # *   Name.
@@ -41,20 +45,19 @@ cell = [
 #
 # *   Position.
 #
-#     Three numbers that define position of the atom in the basis of the unit cell.
+#     Three numbers that define position of the atom in the basis of the unit cell. I.e.
+#     relative positions (can also be called "fractional").
 #
 # *   Spin.
 #
 #     Spin value of the magnetic center. Please note that this is a single number.
-#     The direction of the spin vector or the local quantization axis is put aside of the
-#     crystal as it may be varied.
 #
 # *   g-factor.
 #
 #     Proportionality coefficient for the Zeeman term.
 #
-# Now, magnopy stores and understand a *set* of atoms (even if there is only one atom in the set).
-# It store them as a dictionary, for example
+# Magnopy stores and understand a *set* of atoms (even if there is only one atom in the set).
+# It stores them as a dictionary, for example
 
 
 atoms = {
@@ -73,24 +76,33 @@ atoms = {
 # Magnopy relies on |wulfric|_ for all manipulations with the crystal. In fact, the
 # visualization engine of magnopy (:external:py:class:`magnopy.PlotlyEngine`) is an
 # extension of wulfric's visualization engine (:external:py:class:`wulfric.PlotlyEngine`).
-# They both operate in the same way, the only difference is that magnopy's one have extra
-# methods for plotting spin Hamiltonian and spin directions.
+# They both operate in the same way, the only difference is that magnopy's one has extra
+# methods for plotting spin directions.
 #
-# The recommended scenario for using this kind of visualization is first to create an
-# instance of visualization backend
+# There is a recommended scenario for using this kind of visualization. First, one shall
+# create an instance of visualization backend
 
 pe = magnopy.PlotlyEngine(_sphinx_gallery_fix=True)
 
 # %%
-# And then plot what you want. See API reference for the list of available plotting
-# methods :external:py:class:`magnopy.PlotlyEngine`. For example, to display the cell and
-# atoms
+#
+# .. note::
+#    Please ignore ``_sphinx_gallery_fix=True`` and do not include this argument in you
+#    scripts.
+#
+# Then, plot what you want to plot. See API reference for the list of available plotting
+# methods -  :external:py:class:`magnopy.PlotlyEngine`. For example, to display the cell
+# and atoms
 
 pe.plot_cell(cell, legend_label="Unit cell", color="Black")
 pe.plot_atoms(cell, atoms, legend_label="Atoms of the unit cell")
 
 # %%
-# Finally display or save the figure
+# Finally, display or save the figure
+#
+# .. hint::
+#     Figure are interactive. Try to rotate it. Try to zoom in/out.
+#     Try to click on the elements of the legend.
 
 pe.show(axes_visible=False)
 
