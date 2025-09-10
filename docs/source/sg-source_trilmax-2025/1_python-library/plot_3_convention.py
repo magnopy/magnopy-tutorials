5a579d04ba4ddfb936e44941a511998d93dec7a9
Andrey Rybakov <rybakov.ad@icloud.com>
2025-09-10 10:40:59 +0200

    Refactor

--- docs/source/sg-source_trilmax-2025/1_python-library/plot_3_convention.py
+++ docs/source/sg-source_trilmax-2025/1_python-library/plot_3_convention.py
@@ -6,13 +6,14 @@ Convention of spin Hamiltonian
 
     *   Create an instance of :external:py:class:`magnopy.Convention` for every example
         mathematical formula that is written in this page.
-    *   (extra) Write down the spin Hamiltonian in the convention that you are usually use
+    *   (extra) Write down the spin Hamiltonian in the convention that you usually use
         or seen recently in some paper. Create an instance of the
         :external:py:class:`magnopy.Convention` that describe it.
 
 Before we move on to the Hamiltonian itself it is very important to understand that
-there is a dozen of different conventions of it present in the literature. Here are a
-few examples with different conventions for the isotropic exchange term
+there is a dozen of different conventions of essentially the same type of
+Hamiltonian present in the literature. Here are a few examples of different conventions
+for the isotropic exchange term
 
 .. math::
 
@@ -58,7 +59,7 @@ and user can just read the Hamiltonian from the file like so
     spinham = magnopy.io.load_grogu("spinham-from-GROGU.txt")
 
 However, when the spin Hamiltonian is created by user explicitly, the
-:external:py:class:`magnopy.Convention`. object has to be created. For example to
+:external:py:class:`magnopy.Convention` object has to be created. For example, to
 introduce the convention for the Hamiltonian that is written as
 
 .. math::
@@ -108,9 +109,10 @@ convention.summary()
 
 # %%
 # Modifying convention
+# ====================
 #
-# To get the convention where just some of the properties are changed one can create one
-# from scratch by re-defining every property of it. Or use a shortcut
+# To get the convention where just a few of the properties are changed one can create a
+# new convention and populate all of its properties again. Or use a shortcut
 
 modified = convention.get_modified(c31=1)
 modified.summary()
